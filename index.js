8760e10a06b64035945e5f2b4f1315a639411264
Dip Chondo Partho <dipparthochondo@gmail.com>
2025-06-06 19:50:59 +0600

    add allblog route to fetch all blog data

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000 || process.env.PORT
+
+// middle wares
+
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -22,10 +28,22 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const db = client.db("blogs_data");
+        const collection = db.collection("all_blogs");
+
+        app.get('/recent_blog', async (req, res) => {
+            const data = await collection.find().limit(6).toArray();
+            res.send(data)
+        })
+        app.get('/allblog', async (req, res) => {
+            const data = await collection.find().toArray();
+            res.send(data)
+        })
+
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
     } finally {
 
     }
